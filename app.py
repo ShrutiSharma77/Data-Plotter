6a41baa765eb5a6cb12a35ac18c4137b0e43d3be
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-07 20:56:06 +0530

    added array

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 st.title("Data Plotter")
@@ -9,4 +10,8 @@ xlsx_file = st.file_uploader("Upload XLSX file", type=["xlsx"])
 if xlsx_file is not None:
     df = pd.read_excel(xlsx_file)
     st.write(df)
+    row_index = 0
+    row_values = df.iloc[row_index].to_numpy()
+    
+    st.write("Row Values:", row_values)
 
