e78d9bd1c67abf57044ea359e018497129109975
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-07 19:57:18 +0530

    change added

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ import pandas as pd
 
 st.title("Data Plotter")
 
-# Display a file uploader widget
 xlsx_file = st.file_uploader("Upload XLSX file", type=["xlsx"])
 
 if xlsx_file is not None:
-    # Read the XLSX file into a DataFrame
     df = pd.read_excel(xlsx_file)
-
-    # Display the DataFrame in Streamlit
     st.write(df)
 
