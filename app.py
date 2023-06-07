e9a0a2a8d3c5b1d1d4c841587ce15e54dd1c8766
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-07 19:50:44 +0530

    added requirements

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
-import pandas as pd
+from openpyxl import load_workbook
 
-st.title('Data-Plotter')
-
+# Display a file uploader widget
 xlsx_file = st.file_uploader("Upload XLSX file", type=["xlsx"])
 
 if xlsx_file is not None:
-    df = pd.read_excel(xlsx_file)
-    st.write(df)
+    # Load the XLSX file
+    wb = load_workbook(xlsx_file)
+
+    # Select the first sheet
+    sheet = wb.active
+
+    # Read the data from the sheet
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(row)
+
+    # Display the data in Streamlit
+    st.write(data)
