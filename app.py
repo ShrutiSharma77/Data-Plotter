4a1d43cfa961238453e9b930b69cc2426fc6e606
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-07 21:02:45 +0530

    sum

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ if xlsx_file is not None:
     df = pd.read_excel(xlsx_file)
     st.write(df)
     column_values = df.to_numpy()
+
+    i = 0
+
+    for i in column_values:
+        sum = column_values[i]+column_values[i+1]
+        st.write(sum)
+        sum = 0
+        i = i + 2
     
     st.write("Column Values:", column_values)
