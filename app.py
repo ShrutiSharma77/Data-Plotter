546aff7a4cdbff899f0376a25957726de70b9a58
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-08 12:36:10 +0530

    changed

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import math
 
 st.title("Data Plotter")
 
@@ -13,23 +14,64 @@ if xlsx_file is not None:
     column_values = df.to_numpy()
 
     averages = []
-    
-    for i in range(0,len(column_values)-1,2):
-        avg = (column_values[i]+column_values[i+1])/2
-        print(column_values[i+1])
+
+    for i in range(0, len(column_values) - 1, 2):
+        avg = (column_values[i] + column_values[i + 1]) / 2
         averages.append(avg)
 
-    avg_dia = np.array(averages)   
-    
+    avg_dia = np.array(averages)
+    length = len(avg_dia)
+
     averages_flattened = avg_dia.flatten()
 
     j = 1
 
     st.write("Averages:")
     for value in averages_flattened:
-        st.write(f"{j}.",value)
+        st.write(f"{j}.", value)
         j = j + 1
 
-    range = st.number_input("Enter the range", min_value=0, max_value=100, value=0, step=1)
+    range_value = st.number_input("Enter the range", min_value=0, max_value=100, value=0, step=1)
 
-    st.write("Range:", range)
+    st.write("Range:", range_value)
+
+    max_value = np.max(avg_dia)
+
+    st.write("The maximum value in the array is:", max_value)
+
+    rounded_value = math.ceil(max_value / 10) * 10
+
+    st.write("Rounded value:", rounded_value)
+
+    lower_bound = 0
+    upper_bound = rounded_value
+
+    x = np.array([])
+
+    while lower_bound <= upper_bound:
+        x = np.append(x, lower_bound)
+        lower_bound += range_value
+
+    st.write("X-axis:", x)
+
+    num_ranges = upper_bound // range_value
+
+    count_array = [0] * num_ranges
+
+    for value in avg_dia:
+        lower_bound = int(value) // range_value * range_value
+        upper_bound_range = min(lower_bound + range_value, upper_bound)
+
+        range_index = lower_bound // range_value
+        count_array[range_index] += 1
+
+    y_len = np.array(count_array)
+
+    y = y_len/length
+
+    st.write("Y-axis:",y)
+
+
+    st.title("Bar Graph")
+
+    
